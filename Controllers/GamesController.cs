abd73cf41919c6e0e950c47ce69ae5bf0cc61526
Matej Samler <matej.samler@thenetw.org>
2026-02-24 16:33:05 +0100

    Refactor GamesController, update Index view content, rename Talxis title, and remove unused image files

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using samler.cz.Models;
 
 namespace samler.cz.Controllers
 {
